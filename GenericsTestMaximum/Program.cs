dc91580f2b7d36e72452b318e056841052d252f5
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-18 20:18:24 +0530

    Calculating Maximum values for different data type values

--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -14,6 +14,9 @@ namespace GenericsTestMaximum
             MaxInteger.GivenInteger(20, 30, 40);
             MaxFloatValue.GivenFloat(10.2f, 20.2f, 1.2f);
             MaxStringvalues.GivenString("Hello", "Hi", "Hru");
+            Refactor1<int>.UsingDifferentDatatypesFindMaxValue(10, 20, 30);
+            Refactor1<float>.UsingDifferentDatatypesFindMaxValue(10.5f, 10.55f, 10.555f);
+            Refactor1<string>.UsingDifferentDatatypesFindMaxValue("Welcome","Hru","Fine");
             Console.ReadLine();
         }
     }
---
+++ GenericsTestMaximum/Refactor1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsTestMaximum
+{
+    internal class Refactor1<T> where T : IComparable<T>
+    {
+        public T Firsvalue, SecondValue, ThirdValue;
+        public Refactor1(T Firsvalue, T SecondValue, T ThirdValue)
+        {
+            this.Firsvalue = Firsvalue;
+            this.SecondValue = SecondValue;
+            this.ThirdValue = ThirdValue;
+        }
+        public static T UsingDifferentDatatypesFindMaxValue(T Firstvalue, T Secondvalue, T Thirdvalue)
+        {
+            if (Firstvalue.CompareTo(Secondvalue) > 0 && Firstvalue.CompareTo(Thirdvalue) > 0 ||
+               Firstvalue.CompareTo(Secondvalue) >= 0 && Firstvalue.CompareTo(Thirdvalue) > 0 ||
+               Firstvalue.CompareTo(Secondvalue) > 0 && Firstvalue.CompareTo(Thirdvalue) >= 0)
+            {
+                Console.WriteLine($"First number {Firstvalue} is greater than Second Number {Secondvalue} and ThirdNumber {Thirdvalue}");
+                return Firstvalue;
+            }
+            if (Secondvalue.CompareTo(Firstvalue) > 0 && Secondvalue.CompareTo(Thirdvalue) > 0 ||
+                Secondvalue.CompareTo(Firstvalue) >= 0 && Secondvalue.CompareTo(Thirdvalue) > 0 ||
+                Secondvalue.CompareTo(Firstvalue) > 0 && Secondvalue.CompareTo(Thirdvalue) >= 0)
+            {
+                Console.WriteLine($"Second number {Secondvalue} is greater than First Number {Firstvalue} and ThirdNumber {Thirdvalue}");
+                return Secondvalue;
+            }
+            if (Thirdvalue.CompareTo(Firstvalue) > 0 && Thirdvalue.CompareTo(Secondvalue) > 0 ||
+               Thirdvalue.CompareTo(Firstvalue) >= 0 && Thirdvalue.CompareTo(Secondvalue) > 0 ||
+               Thirdvalue.CompareTo(Firstvalue) > 0 && Thirdvalue.CompareTo(Secondvalue) >= 0)
+            {
+                Console.WriteLine($"Third number {Thirdvalue} is greater than First Number {Firstvalue} and Second Number {Secondvalue}");
+                return Thirdvalue;
+            }
+            return Firstvalue;
+        }
+    }
+}

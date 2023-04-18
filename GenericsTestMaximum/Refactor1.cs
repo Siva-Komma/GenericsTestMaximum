6ad6c3237a7f61dbc3a76bf8f871a3fc4c281114
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-18 20:20:30 +0530

    Calculating Maximum values for different data type values

--- GenericsTestMaximum/Refactor1.cs
+++ GenericsTestMaximum/Refactor1.cs
@@ -21,21 +21,21 @@ namespace GenericsTestMaximum
                Firstvalue.CompareTo(Secondvalue) >= 0 && Firstvalue.CompareTo(Thirdvalue) > 0 ||
                Firstvalue.CompareTo(Secondvalue) > 0 && Firstvalue.CompareTo(Thirdvalue) >= 0)
             {
-                Console.WriteLine($"First number {Firstvalue} is greater than Second Number {Secondvalue} and ThirdNumber {Thirdvalue}");
+                Console.WriteLine($"First value {Firstvalue} is greater than Second value {Secondvalue} and Third value {Thirdvalue}");
                 return Firstvalue;
             }
             if (Secondvalue.CompareTo(Firstvalue) > 0 && Secondvalue.CompareTo(Thirdvalue) > 0 ||
                 Secondvalue.CompareTo(Firstvalue) >= 0 && Secondvalue.CompareTo(Thirdvalue) > 0 ||
                 Secondvalue.CompareTo(Firstvalue) > 0 && Secondvalue.CompareTo(Thirdvalue) >= 0)
             {
-                Console.WriteLine($"Second number {Secondvalue} is greater than First Number {Firstvalue} and ThirdNumber {Thirdvalue}");
+                Console.WriteLine($"Second value {Secondvalue} is greater than First value {Firstvalue} and Third value {Thirdvalue}");
                 return Secondvalue;
             }
             if (Thirdvalue.CompareTo(Firstvalue) > 0 && Thirdvalue.CompareTo(Secondvalue) > 0 ||
                Thirdvalue.CompareTo(Firstvalue) >= 0 && Thirdvalue.CompareTo(Secondvalue) > 0 ||
                Thirdvalue.CompareTo(Firstvalue) > 0 && Thirdvalue.CompareTo(Secondvalue) >= 0)
             {
-                Console.WriteLine($"Third number {Thirdvalue} is greater than First Number {Firstvalue} and Second Number {Secondvalue}");
+                Console.WriteLine($"Third value {Thirdvalue} is greater than First value {Firstvalue} and Second value {Secondvalue}");
                 return Thirdvalue;
             }
             return Firstvalue;
